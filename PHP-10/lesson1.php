248c7d80902b89b3203a7fe1d4b9b8e4a1b064f4
natsuki-oikawa <151366290+natsuki-oikawa@users.noreply.github.com>
2024-01-08 00:21:26 +0900

    Update lesson1.php

--- PHP-10/lesson1.php
+++ PHP-10/lesson1.php
@@ -3,7 +3,7 @@
 // 氏名入力時に入力内容が表示されるようにプログラムを完成させてください。
 // プログラム内にバグが含まれているので正常に動くように修正してください。
 
-if (empty($_POST)) {
+if (!empty($_POST)) {
     $lastName = $_POST['last_name'];
     $firstName= $_POST['first_name'];
     if ($lastName != null && $firstName != null) {
@@ -15,17 +15,17 @@ if (empty($_POST)) {
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>デバック練習</title>
+    <title>デバック練習</title>
 </head>
 <body>
-    <section>
-    <form action='./lesson.php' method="post">
-        <label>姓</label>
-        <input type="text" name="last_name"/>
-        <label>名</label>
-        <input type="text" name="first_nae" />
-        <input type="submit" value="送信する"/>
+<section>
+<form action = "./lesson1.php" method="post">
+        <label>姓 :</label>
+        <input type="text" name="last_name">
+        <label>名 :</label>
+        <input type="text" name="first_name" >
+        <input type="submit" value="送信する">
     </form>
-    </section>
+</section>
 </body>
 </html>
